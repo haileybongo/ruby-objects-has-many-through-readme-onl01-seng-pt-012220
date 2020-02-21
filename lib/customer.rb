6925a922fde2ef09ecc625f767fe9b44e8926a54
Hailey Bongo <haileyebongo@gmail.com>
2020-02-21 21:17:59 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -22,6 +22,14 @@ class Customer
   def meals
   Meal.all.select do |meal|
     meal.customer == self
+    end
   end
-end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+   end
+  end
+  
+  
 end
